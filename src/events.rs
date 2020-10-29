098868b81d6c26da67f8a96511b34c86712cd476
Samuli Lehtonen <cr0xxy94@gmail.com>
2020-10-30 00:43:14 +0200

    Use prettytable

--- src/events.rs
+++ src/events.rs
@@ -1,9 +1,24 @@
 use crate::db;
 
+use prettytable::Table;
+
 pub fn print_events() {
     let events = db::load_events();
+    let mut ptable = Table::new();
+    ptable.set_titles(row!["Id", "Date", "Description", "From", "To", "Account", "Sum"]);
+    ptable.set_format(*prettytable::format::consts::FORMAT_NO_LINESEP_WITH_TITLE);
 
     for event in events.iter() {
-        println!("{0} | {1}", event.event_id, event.event_date);
+        let eid = &event.event_id;
+        let date = &event.event_date;
+        let desc = &event.event_desc;
+        let from = &event.event_from;
+        let to = &event.event_to;
+        let acc = &event.event_acc;
+        let sum_str = ((event.event_sum as f32) / 100.0).to_string() + " €";
+
+        ptable.add_row(row![eid, date, desc, from, to, acc, sum_str]);
     }
+
+    ptable.printstd();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 #[macro_use]
+extern crate prettytable;
+
+#[macro_use]
 extern crate diesel;
 
 mod db;
